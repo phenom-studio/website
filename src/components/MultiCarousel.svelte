30fe1ac9ce9eb3cfebad130fe48d7edb48532593
jill <jill@phenom.studio>
2021-02-15 19:09:53 -0800

    added MultiCarousel carousel container

---
+++ src/components/MultiCarousel.svelte
@@ -0,0 +1,19 @@
+<script>
+  import Carousel from 'Carousel.svelte'
+
+  export let carouselImages;
+
+</script>
+
+<Carousel 
+    {carouselImages}
+/>
+<Carousel 
+    {carouselImages}
+/>
+<Carousel 
+    {carouselImages}
+/>
+<Carousel 
+    {carouselImages}
+/>
